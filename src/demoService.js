697bb76381f6bb6fd336b7c72427d775cc1b32d9
yeforriak <yeforriak@gmail.com>
2021-08-25 13:47:00 +0200

    feat: add new to millis endpoint

--- src/demoService.js
+++ src/demoService.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const ms = require('ms')
 const app = express()
 const port = 3000
 
 app.get('/hello', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/toMillis', (req, res) => {
+  const period = req.query.period
+  const millis = ms(period)
+  res.send(`${period} = ${millis} millis`)
 })
 
 app.listen(port, () => {
